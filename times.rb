ef7729c19a853b3a3bb14571235f1f2311872955
Munezero Christian <chamsf5@aol.com>
2022-06-20 11:51:29 +0200

    Debug with pry

--- times.rb
+++ times.rb
@@ -1,7 +1,7 @@
 def times_two(arr)
-    arr.map do | number |
-        number.map { |num| puts num * 2}
-    end
+  arr.map do |number|
+    number.map { |num| puts num * 2 }
+  end
 end
 
-times_two [[1,2,3],[4,5,6],[7,8,9]]
+times_two [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
