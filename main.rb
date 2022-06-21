029c9a4d6879a1073f5ac20617cfe4ef2847e030
Munezero Christian <chamsf5@aol.com>
2022-06-21 11:30:23 +0200

    Refactor app.rb

--- main.rb
+++ main.rb
@@ -1,8 +1,27 @@
 require_relative './app'
 
+$app = App.new
+
 def main
-  app = App.new
-  app.run
+  puts ''
+  puts 'Welcome to school libray app!'
+  puts 'Please choose an option by entering a number:'
+  options
+end
+
+def options
+  puts '
+    1 - List all books
+    2 - List all people
+    3 - Create a person
+    4 - Create a book
+    5 - Create a rental
+    6 - list all rentals for a given person id
+    7 - Exit
+    '
+  print 'Select an Option: '
+  user_input = gets.chomp
+  $app.get_option user_input
 end
 
 main
