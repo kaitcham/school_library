03b0c2bccf06c87d97d78f009c8bebe41cc557d7
Munezero Christian <chamsf5@aol.com>
2022-06-23 15:39:48 +0200

    Modified files to preserve data

--- person.rb
+++ person.rb
@@ -1,36 +1,46 @@
-require_relative './nameable'
+require_relative './corrector'
+require_relative './rental'
 
-class Person < Nameable
-  attr_accessor :name, :age
-  attr_reader :id, :rental
-
+class Person
+  @@id = 1
   def initialize(age, name = 'Unknown', parent_permission: true)
-    super()
-    @id = Random.rand(1..1000)
+    @id = @@id
+    @@id += 1
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @rental = []
+    @corrector = Corrector.new
+    @rentals = []
   end
+
+  attr_accessor :name, :age
+  attr_reader :id, :rentals, :parent_permission
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def validate_name
+    @name = @corrector.correct_name(@name)
+  end
+
+  def add_rental(book, date)
+    Rental.new(date, self, book)
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => @id,
+      'name' => @name,
+      'age' => @age,
+      'parent_permission' => @parent_permission
+    }.to_json(*args)
+  end
+
+  private
 
   def of_age?
     @age >= 18
   end
-
-  private :of_age?
-
-  def can_use_services?
-    of_age || parent_permission == true
-  end
-
-  def correct_name
-    @name
-  end
-
-  def add_rental(book, date)
-    Rental.new(date, book, self)
-  end
 end
-
-# # person = Person.new(22, 'maximilianus')
-# # p person.correct_name
--- student.rb
+++ student.rb
@@ -1,21 +1,15 @@
-require_relative './person'
-require_relative './classroom'
+require './person'
 
 class Student < Person
-  attr_reader :classroom
+  attr_reader :classroom, :parent_permission
 
-  # rubocop: disable Style/OptionalBooleanParameter
-
-  def initialize(age, classroom, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    @classroom = classroom
   end
 
-  # rubocop: enable Style/OptionalBooleanParameter
-
-  def classroom=(classroom)
-    @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
+  def classroom=(studentclass)
+    @classroom = studentclass
+    studentclass.students.push(self) unless studentclass.students.include?(self)
   end
 
   def play_hooky
