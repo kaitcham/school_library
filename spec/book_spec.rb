bed6b67975b144e99a88ccf6189db6ae9d4e96d9
Munezero Christian <chamsf5@aol.com>
2022-06-23 17:22:05 +0200

    Add tests for classes

---
+++ spec/book_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../book'
+
+describe Book do
+  context 'When testing the Book class' do
+    title = 'Harry Potter'
+    author = 'JK Rowling'
+    book = Book.new(title, author)
+
+    it 'should validate the title of the book' do
+      expect(book.title).to eq title
+    end
+
+    it 'should validate the author of the book' do
+      expect(book.author).to eq author
+    end
+  end
+end
