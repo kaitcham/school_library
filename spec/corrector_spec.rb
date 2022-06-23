eaf06a3cbf0959e138bc0b91de99675f6c17471a
Ezekiel Utshudi Eteta <ezekielutshudi@gmail.com>
2022-06-23 17:25:37 +0200

    Update corrector_spec.rb

--- spec/corrector_spec.rb
+++ spec/corrector_spec.rb
@@ -1,3 +1,13 @@
 require_relative '../corrector'
 
+describe Corrector do
+  context 'When testing the Corrector class' do
+    corrector = Corrector.new
 
+    it 'the method correct_name capitalize the name and trim to 10 length' do
+      name = 'kaiittttttt'
+      corrected_name = corrector.correct_name(name)
+      expect(corrected_name).to eq 'Kaiitttttt'
+    end
+  end
+end
